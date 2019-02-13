23d14611eb5d78bc2e82c89477f1894166b44349
Jason Wells <jason.wells@nuix.com>
2019-02-13 12:02:51 -0800

    Update DiagnosticsTest.rb
    added comments

--- RubyTests/DiagnosticsTest.rb
+++ RubyTests/DiagnosticsTest.rb
@@ -1,6 +1,10 @@
+# Load up the JAR
 script_directory = File.dirname(__FILE__)
 require File.join(script_directory,"SuperUtilities.jar")
 java_import com.nuix.superutilities.SuperUtilities
+
+# Initialize super utilities
 $su = SuperUtilities.init($utilities,NUIX_VERSION)
 
+# Use super utilities to save a Nuix diagnostics file
 $su.saveDiagnostics("C:\\Temp\\DiagnosticsTest.zip")
