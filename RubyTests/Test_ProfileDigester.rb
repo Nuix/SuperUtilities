30dc9e77fdf8296aca85c83072a9debdc4f058f4
Jason Wells <jason.wells@nuix.com>
2019-08-22 16:28:46 -0700

    Created ProfileDigester
    Created class ProfileDigester which offers a few handy methods regarding MD5 digest generation using the concatenated field values of a metadata profile evaluated against a given item.  Includes a helper method to add items to an item set using "Scripted" deduplication using these digests, effectively allowing you to create an item set which is deduplicated by the values yielded by a metadata profile for each item being added to that item set.

---
+++ RubyTests/Test_ProfileDigester.rb
@@ -0,0 +1,76 @@
+script_directory = File.dirname(__FILE__)
+require File.join(script_directory,"SuperUtilities.jar")
+java_import com.nuix.superutilities.SuperUtilities
+$su = SuperUtilities.init($utilities,NUIX_VERSION)
+java_import com.nuix.superutilities.misc.ProfileDigester
+
+$current_case = $utilities.getCaseFactory.open("D:\\cases\\FakeData_8.0")
+
+profile_digester = ProfileDigester.new
+
+items = $current_case.search("kind:email")
+
+profile = $utilities.getMetadataProfileStore.createMetadataProfile
+profile = profile.addMetadata("SPECIAL","Name")
+profile = profile.addMetadata("SPECIAL","Kind")
+
+profile_digester.setProfile(profile)
+
+concat_grouped = Hash.new{|h,k| h[k] = [] }
+hash_grouped = Hash.new{|h,k| h[k] = [] }
+
+items.each do |item|
+	concat = profile.getMetadata.map{|field| field.evaluate(item)}.join
+	hash = profile_digester.generateMd5String(item)
+
+	concat_grouped[concat] << item
+	hash_grouped[hash] << item
+end
+
+if concat_grouped.size == hash_grouped.size
+	puts "Group sizes match"
+
+
+	# Ruby iterates a hash in key insertion order so I believe each
+	# entry should correspond to the same "dedupe group".  We will peel
+	# off 1 group from each and make sure the concatenation based key
+	# grouped up the same number of input items as the 
+	cge = concat_grouped.to_enum
+	hge = hash_grouped.to_enum
+
+	matched = 0
+	mismatched = 0
+
+	concat_grouped.size.times do |iteration|
+		c = cge.next
+		h = hge.next
+
+		if c[1].size != h[1].size
+			puts "="*20
+			puts "#{c[0]} => #{c[1].size}"
+			puts "#{h[0]} => #{h[1].size}"
+			"Concat had #{c[1].size} for group but hash had #{h[1].size} for same group"
+			mismatched += 1
+		else
+			matched += 1
+		end
+	end
+
+	puts "Matched: #{matched}"
+	puts "Mismatched: #{mismatched}"
+
+	# Now we put same items into an item set using ProfileDigester, we expect that the
+	# total number of originals in that item set should match the number of groups,
+	# 1 original per group, when deduping by individual
+	item_set_name = "Profile Debugger #{Time.now.to_i}"
+	dedupe_by = "INDIVIDUAL"
+	item_set = profile_digester.addItemsToItemSet($current_case,item_set_name,dedupe_by,items)
+	puts "Item Set Originals: #{item_set.getOriginals.size}, Expected: #{concat_grouped.size}"
+	# We expect that duplicates will be items count - originals count
+	puts "Item Set Duplicates: #{item_set.getDuplicates.size}, Expected: #{items.size - concat_grouped.size}"
+else
+	puts "Sizes don't match!"
+end
+
+
+$current_case.close
